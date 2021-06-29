d63ebd7f56c12ff9e9882ac72786ff35d318fa19
aschvin <aschvin.umachandran@edu.tbz.ch>
2021-06-29 16:24:42 +0200

    Better text

--- lb02/lb02-web-app/src/data/InfoData.js
+++ lb02/lb02-web-app/src/data/InfoData.js
@@ -4,9 +4,9 @@ export const InfoData =  {
     heading: "Neueste learnings",
     paragraphOne: "Gelernte Animationen anwenden können",
     paragraphTwo: "Was war neu: Ich dachte, dass man in einem Framework die Basics nicht mehr braucht, jedoch habe ich mich " +
-        "hier sehr getäuscht. Ich bin froh, dass ich im Modul noch einige Wissenslücken füllen konnte und nun bereit bin, auch " +
-        "ein guter Front End developer zu sein. Vorallem habe ich gelernt, dass Animationen sehr viel ausmachen in einer Webseite und " +
-        "auch Übung brauchen. Ich habe mehrere Wochen verbracht, diese Webseite zu erstellen. Ich scheiterte oft an kleinen Dingen. Ich habe auch " +
+        "bei dieser These sehr getäuscht. Ich bin froh, dass ich im Modul noch einige Wissenslücken füllen konnte und nun bereit bin, auch " +
+        "ein guter Frontend Developer zu sein. Vorallem habe ich gelernt, dass Animationen zu implementieren sehr viel ausmachen in einer Webseite und " +
+        "auch viel Übung brauchen. Ich habe mehrere Wochen verbracht, diese Webseite zu erstellen. Ich scheiterte oft an kleinen Dingen. Ich habe auch " +
         "mehrere Tage gebraucht, um diese ScrollAnimation zu verstehen. Die Anwendung ist eher einfach, aber das Verständnis dahinter hat mich viel " +
         "Zeit gekostet.",
     buttonLabel: "View more",
